a1cdcac157b19749716b6a88447e0b6ec4454bfc
Patrol220 <22000patrol@gmail.com>
2023-04-20 20:05:45 +0200

    aht20 module handling, real humidity value in weather data provider

--- lcd/app/provider/WeatherDataProvider.py
+++ lcd/app/provider/WeatherDataProvider.py
@@ -1,24 +1,33 @@
 from adafruit_bmp280 import Adafruit_BMP280_I2C
+from adafruit_ahtx0 import AHTx0
 import board
 
 
 class WeatherDataProvider:
-    weather_module: Adafruit_BMP280_I2C
+    weather_module_bmp280: Adafruit_BMP280_I2C
+    weather_module_aht20: AHTx0
 
     def __init__(self):
-        self.weather_module = self.__init_weather_module()
+        self.weather_module_bmp280 = self.__init_weather_module_bmp280()
+        self.weather_module_aht20 = self.__init_weather_module_aht20()
 
-    def __init_weather_module(self) -> Adafruit_BMP280_I2C:
+    def __init_weather_module_bmp280(self) -> Adafruit_BMP280_I2C:
         i2c = board.I2C()
         bmp280 = Adafruit_BMP280_I2C(i2c)
 
         return bmp280
 
-    def get_temperature(self):
-        return self.weather_module.temperature
+    def __init_weather_module_aht20(self) -> AHTx0:
+        i2c = board.I2C()
+        aht20 = AHTx0(i2c)
 
-    def get_humidity(self):
-        return 3
+        return aht20
 
-    def get_pressure(self):
-        return self.weather_module.pressure
+    def get_temperature(self) -> float:
+        return self.weather_module_bmp280.temperature
+
+    def get_humidity(self) -> int:
+        return self.weather_module_aht20.relative_humidity
+
+    def get_pressure(self) -> float:
+        return self.weather_module_bmp280.pressure
